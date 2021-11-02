ec9625d5578a24507ae4808e014383c5c4b99245
Nikita <nikitaaa.dr@gmail.com>
2021-11-02 17:41:55 +0300

    fixed tests

--- src/Algorithms/3.3/multiplyAll.test.js
+++ src/Algorithms/3.3/multiplyAll.test.js
@@ -1,7 +1,7 @@
 import { multiplyAll } from './multiplyAll';
 
 test('Умножение всех элементов массива', () => {
-  expect(multiplyAll([1, 2, 3])(1)).toBe([1, 2, 3]);
-  expect(multiplyAll([1, 2, 3])(2)).toBe([2, 4, 6]);
-  expect(multiplyAll([1, 2, 3])(0)).toBe([0, 0, 0]);
+  expect(multiplyAll([1, 2, 3])(1)).toStrictEqual([1, 2, 3]);
+  expect(multiplyAll([1, 2, 3])(2)).toStrictEqual([2, 4, 6]);
+  expect(multiplyAll([1, 2, 3])(0)).toStrictEqual([0, 0, 0]);
 });
--- src/Algorithms/3.6/compose.test.js
+++ src/Algorithms/3.6/compose.test.js
@@ -3,8 +3,8 @@ const addOne = (a) => a + 1;
 const multTwo = (b) => b * 2;
 
 test('Функциональная композиция', () => {
-  expect(compose(compose(multTwo, addOne)(5))).toBe(12);
-  expect(compose(compose(addOne, multTwo, addOne, addOne)(2))).toBe(9);
-  expect(compose(compose(addOne)(3))).toBe(4);
-  expect(compose(compose()(10))).toBe(10);
+  expect(compose(multTwo, addOne)(5)).toBe(12);
+  expect(compose(addOne, multTwo, addOne, addOne)(2)).toBe(9);
+  expect(compose(addOne)(3)).toBe(4);
+  expect(compose()(10)).toBe(10);
 });
